cff20dc5d121d7b404b3c29a0237f7241748c823
Ferronn <ferronn@ferronn.dev>
2021-03-21 14:31:30 -0500

    attempt to retry on svn failure

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ Path('/tmp/build.ninja').write_text('\n'.join([
         'if [ -f $out/build.yaml ]; then (cd $out && sh /addonmaker/main.sh); fi',
     '',
     'rule svn',
-    '  command = svn checkout $repo $out',
+    '  command = while ! svn checkout $repo $out; do sleep 5; done',
     '',
     'rule sql',
     '  command = env GOOGLE_APPLICATION_CREDENTIALS=/addonmaker/creds.json ' +
