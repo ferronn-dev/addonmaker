be3e8353ab3c75b033470abc9ee4e5db1770545a
Ferronn <ferronn@ferronn.dev>
2021-03-27 21:24:42 +0000

    better error handling in getlib.sh

--- getlib.sh
+++ getlib.sh
@@ -1,21 +1,29 @@
 repo="$1"
 out="$2"
-fetch() (
-  set -e
+fetch() {
   if echo "$repo" | grep -q "/trunk\(/\|\$\)\|/tags/"
   then
     repo=$(echo "$repo" | sed 's/svn:\/\/svn\.wowace\./https:\/\/repos\.wowace\./' | sed s'/\/mainline\//\//')
-    svn checkout "$repo" "$out"
+    if ! svn checkout "$repo" "$out"
+    then
+      return 1
+    fi
   else
-    git clone --recurse-submodules "$repo" "$out"
+    if ! git clone --recurse-submodules "$repo" "$out"
+    then
+      return 1
+    fi
   fi
   if [ -f "$out/build.yaml" -o -f "$out/.pkgmeta" ]
   then
     (cd "$out" && sh /addonmaker/main.sh)
+    return "$?"
+  else
+    return 0
   fi
-)
+}
 while ! fetch
 do
-    rm -rf "$out"
-    sleep 5
+  rm -rf "$out"
+  sleep 5
 done
