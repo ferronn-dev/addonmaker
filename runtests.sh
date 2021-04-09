d25747ce2319b4c27161de6f316c23ef0a5cbe9f
Ferronn <ferronn@ferronn.dev>
2021-04-09 19:26:23 -0400

    run luacheck before tests

--- runtests.sh
+++ runtests.sh
@@ -1,9 +1,9 @@
 set -e
 eval `luarocks path`
 export LUA_PATH="/addonmaker/?.lua;$LUA_PATH"
+luacheck -q --config /addonmaker/luacheckrc.lua *.lua
 compgen -G "*test.lua" | while read test; do
   echo "******** $test *******"
   lua $test
 done
 compgen -G "*_spec.lua" | xargs -r busted --helper=/addonmaker/helper.lua
-luacheck -q --config /addonmaker/luacheckrc.lua *.lua
