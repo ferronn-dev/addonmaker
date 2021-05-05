fdff87a6f41eebf26592b50d52979b432f735c15
Ferronn <ferronn@ferronn.dev>
2021-05-05 17:04:49 +0000

    generate luacheckrc.lua

---
+++ gencheck.lua
@@ -0,0 +1,27 @@
+print([[-- luacheck: globals files globals read_globals std
+files['*_spec.lua'] = {
+  std = '+busted',
+  globals = {'wow'},
+}
+std = 'min'
+read_globals = {]])
+
+local symbols = {
+  'ChatThrottleLib',
+  'LibStub',
+}
+for k in pairs(require('wow')({})) do
+  if k ~= 'StaticPopupDialogs' then
+    table.insert(symbols, k)
+  end
+end
+table.sort(symbols)
+for _, k in ipairs(symbols) do
+  print("  '" .. k .. "',")
+end
+
+print([[}
+globals = {
+  'SlashCmdList',
+  'StaticPopupDialogs',
+}]])
