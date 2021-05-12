275e6f31f3e1f2c3af6ffd7f2e54d7e63828e6f1
Ferronn <ferronn@ferronn.dev>
2021-05-11 22:13:26 -0500

    numeric release tags

--- release.sh
+++ release.sh
@@ -3,21 +3,25 @@ set -e
 file=$(echo *.zip)
 mkdir -p /tmp/rel/old
 unzip -q -d /tmp/rel/new "$file"
-if gh release download -D /tmp/rel/rel latest; then
-  echo 'Checking existing release...'
+echo 'Checking for existing release...'
+if tag=$(gh api repos/:owner/:repo/releases/latest --jq .tag_name); then
+  echo "Found release '$tag'. Checking for diffs..."
+  gh release download -D /tmp/rel/rel "$tag"
   unzip -q -d /tmp/rel/old "/tmp/rel/rel/$file"
   if diff -qrN /tmp/rel/old /tmp/rel/new; then
     echo 'No difference since last release.'
     exit 0
   fi
-  echo 'Deleting existing release...'
-  gh release delete latest < /dev/null
+else
+  echo 'No existing release.'
+  tag=v0
 fi
-echo 'Creating new release...'
+newtag=v$(($(echo "$tag" | tr -d v) + 1))
+echo "Creating new release '$newtag'..."
 (cd /tmp/rel;
  echo '```';
  git diff --no-index --stat=120 old new || true;
  echo '```') > /tmp/thediff.md
-gh release create -t latest -F /tmp/thediff.md latest
-gh release upload latest "$file"
+gh release create -t "$newtag" -F /tmp/thediff.md "$newtag"
+gh release upload "$newtag" "$file"
 echo 'Done.'
