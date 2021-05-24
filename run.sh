5748b05f645c617beccd43674d11bddeb6e95cd2
Ferronn <ferronn@ferronn.dev>
2021-05-24 19:41:24 +0000

    tag images when using cache

--- run.sh
+++ run.sh
@@ -2,12 +2,14 @@ set -e
 echo 'Building addonmaker...'
 if [ -z $ADDONMAKER_BUILDCACHE ]; then
   docker buildx build --iidfile=/tmp/iid.$$ --load addonmaker
+  image=$(cat /tmp/iid.$$)
 else
-  docker buildx build --iidfile=/tmp/iid.$$ --load \
-  --cache-from=type=local,src=$ADDONMAKER_BUILDCACHE \
-  --cache-to=type=local,dest=$ADDONMAKER_BUILDCACHE \
-  addonmaker
+  image=addonmaker.$$
+  docker buildx build -t $image --load \
+      --cache-from=type=local,src=$ADDONMAKER_BUILDCACHE \
+      --cache-to=type=local,dest=$ADDONMAKER_BUILDCACHE \
+      addonmaker
 fi
 echo 'Running addonmaker...'
-docker run --rm -t -v "$PWD:/addon" -u `id -u`:`id -g` --net host $(cat /tmp/iid.$$) "$@"
+docker run --rm -t -v "$PWD:/addon" -u `id -u`:`id -g` --net host $image "$@"
 echo 'Done.'
