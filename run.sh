c2369149b26ee1b47f398f281aaaab585ba69904
Ferronn <ferronn@ferronn.dev>
2021-05-24 19:35:46 +0000

    trying hack for build cache

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 set -e
 echo 'Building addonmaker...'
-docker buildx build --iidfile=/tmp/iid.$$ --load addonmaker
+if [ -z $ADDONMAKER_BUILDCACHE ]; then
+  docker buildx build --iidfile=/tmp/iid.$$ --load addonmaker
+else
+  docker buildx build --iidfile=/tmp/iid.$$ --load \
+  --cache-from=type=local,src=$ADDONMAKER_BUILDCACHE \
+  --cache-to=type=local,dest=$ADDONMAKER_BUILDCACHE \
+  addonmaker
+fi
 echo 'Running addonmaker...'
 docker run --rm -t -v "$PWD:/addon" -u `id -u`:`id -g` --net host $(cat /tmp/iid.$$) "$@"
 echo 'Done.'
