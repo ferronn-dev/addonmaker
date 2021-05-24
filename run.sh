b262cda17a28ff0752885769c86870f6031b40cc
Ferronn <ferronn@ferronn.dev>
2021-05-24 18:32:57 +0000

    drop buildx cache for now, doesnt work in cloud shell

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 set -e
-tag="$(basename $PWD)-maker"
 echo 'Building addonmaker...'
-if ! docker buildx build -t "$tag" --cache-from=type=local,src=$HOME/.docker-cache --cache-to=type=local,mode=max,dest=$HOME/.docker-cache addonmaker 2>/tmp/buildx.err.$$; then
-  cat /tmp/buildx.err.$$
-  rm /tmp/buildx.err.$$
-  exit 1
-fi
+docker buildx build --iidfile=/tmp/iid.$$ --load addonmaker
 echo 'Running addonmaker...'
-docker run --rm -t -v "$PWD:/addon" -u `id -u`:`id -g` --net host "$tag"
+docker run --rm -t -v "$PWD:/addon" -u `id -u`:`id -g` --net host $(cat /tmp/iid.$$)
 echo 'Done.'
