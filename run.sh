4b0c5651eaba325011f54d66dc7aadc1a37834b4
Ferronn <ferronn@ferronn.dev>
2021-06-06 11:09:05 -0500

    specify amd64 platform, minimum change to get builds working on apple m1

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 set -e
 echo 'Building addonmaker...'
 if [ -z $ADDONMAKER_BUILDCACHE ]; then
-  docker buildx build --iidfile=/tmp/iid.$$ --load addonmaker
+  docker buildx build --platform linux/amd64 --iidfile=/tmp/iid.$$ --load addonmaker
   image=$(cat /tmp/iid.$$)
 else
   image=addonmaker.$$
-  docker buildx build -t $image --load \
+  docker buildx build --platform linux/amd64 -t $image --load \
       --cache-from=type=local,src=$ADDONMAKER_BUILDCACHE \
       --cache-to=type=local,dest=$ADDONMAKER_BUILDCACHE \
       addonmaker
 fi
 echo 'Running addonmaker...'
-docker run --rm -t -v "$PWD:/addon" -u `id -u`:`id -g` --net host $image "$@"
+docker run --platform linux/amd64 --rm -t -v "$PWD:/addon" -u `id -u`:`id -g` --net host $image "$@"
 echo 'Done.'
