5f5db81ebc32bf80eef5aaac0c0addd561f58b3d
Ferronn <ferronn@ferronn.dev>
2021-06-08 19:41:08 +0000

    use find to get all luas

--- runtests.sh
+++ runtests.sh
@@ -1,11 +1,12 @@
 set -e
 eval `luarocks path`
 export LUA_PATH="/addonmaker/?.lua;$LUA_PATH"
-luacheck -q --config /addonmaker/luacheckrc.lua *.lua
-compgen -G "*test.lua" | while read test; do
+function luas { find -name "$1" -not -path './libs/*' -not -path './addonmaker/*' ; }
+luas '*.lua' | xargs luacheck -q --config /addonmaker/luacheckrc.lua
+luas '*test.lua' | while read test; do
   echo "******** $test *******"
   lua $test
 done
 for toc in *.toc; do
-  compgen -G "*_spec.lua" | xargs -r busted -v --helper=/addonmaker/helper.lua -Xhelper "$toc"
+  luas '*_spec.lua' | xargs -r busted -v --helper=/addonmaker/helper.lua -Xhelper "$toc"
 done
