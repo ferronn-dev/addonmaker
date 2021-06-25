7d2eb505c9c06bb364d77bce8d7197750ef50393
Ferronn <ferronn@ferronn.dev>
2021-06-25 16:45:57 -0500

    fix interaction of old testing.lua with addonloader

--- testing.lua
+++ testing.lua
@@ -16,8 +16,11 @@ return {
       print(toc)
       local failed = 0
       for i, test in ipairs(tests) do
-        local state, env, addon = loader(tests.before, toc)
-        local success, err = pcall(test, state, nil, addon, env)
+        local wow = loader({
+          before = tests.before,
+          toc = toc,
+        })
+        local success, err = pcall(test, wow.state, nil, wow.addon, wow.env)
         if not success then
           failed = failed + 1
           print(i .. ': ' .. err)
