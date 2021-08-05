0c1177790287af4bab9be56299c7dfc35c44e042
Ferronn <ferronn@ferronn.dev>
2021-08-04 20:55:29 -0500

    put main.sh pid in ninja build file name

--- main.sh
+++ main.sh
@@ -1,10 +1,11 @@
 set -e
-python3 /addonmaker/build.py > /tmp/build.ninja
+BUILD=/tmp/build.ninja.$$
+python3 /addonmaker/build.py > $BUILD
 if [ "$1" = "clean" ]; then
   rm -rf libs
-  ninja -f /tmp/build.ninja -t clean
+  ninja -f $BUILD -t clean
 else
-  ninja -f /tmp/build.ninja
+  ninja -f $BUILD
   if [ "$1" = "release" ]; then
     env GITHUB_TOKEN="$2" sh /addonmaker/release.sh
   fi
