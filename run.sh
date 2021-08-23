992848d2744f742e90e7f72107dd8190ffbaa3ef
Ferronn <ferronn@ferronn.dev>
2021-08-23 14:13:58 +0000

    add ADDONMAKER_IMAGE env to run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 set -e
 echo 'Building addonmaker...'
-if [ -z $ADDONMAKER_BUILDCACHE ]; then
-  docker buildx build --iidfile=/tmp/iid.$$ --load addonmaker
-  image=$(cat /tmp/iid.$$)
-else
+if [ -n $ADDONMAKER_IMAGE ]; then
+  image=$ADDONMAKER_IMAGE
+elif [ -n $ADDONMAKER_BUILDCACHE ]; then
   image=addonmaker.$$
   docker buildx build -t $image --load \
       --cache-from=type=local,src=$ADDONMAKER_BUILDCACHE \
       --cache-to=type=local,dest=$ADDONMAKER_BUILDCACHE \
       addonmaker
+else
+  docker buildx build --iidfile=/tmp/iid.$$ --load addonmaker
+  image=$(cat /tmp/iid.$$)
 fi
 echo 'Running addonmaker...'
 docker run --rm -t -v "$PWD:/addon" -u `id -u`:`id -g` --net host $image "$@"
