34d2c607eccf8833bbbf34a340b1f2632fae8ef8
Ferronn <ferronn@ferronn.dev>
2022-06-18 12:58:29 +0000

    package up bindings.xml

--- genzip.py
+++ genzip.py
@@ -5,12 +5,18 @@ import zipfile
 
 addon = sys.argv[1]
 tocs = [pathlib.Path(f) for f in sys.argv[2:]]
-
-files = sorted(set([str(f) for f in tocs] + [
+tocfiles = [
     line
     for toc in tocs
     for line in toc.read_text().splitlines() if not line.startswith('#')
-]))
+]
+others = [
+    f
+    for f in ['bindings.xml']
+    if pathlib.Path(f).exists()
+]
+
+files = sorted(set([str(f) for f in tocs] + tocfiles + others))
 with zipfile.ZipFile(f'{addon}.zip', 'w') as zf:
     for f in files:
         zf.write(f, f'{addon}/{f}')
